84eef8d2129ebc9a32cfe01b0fde6804326eb055
Samuel Walker <Samuel Walker>
2016-02-24 16:37:20 -0700

    map setup stuff

---
+++ Assets/Scripts/data/Tile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum TileType { Empty, Dirt, Grass, Floor_Wood, Wall_Brick }
+
+public class Tile {
+
+    World world;
+    int x;
+    int y;
+    TileType type;
+
+    public Tile(int x, int y, World world) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.type = TileType.Empty;
+    }
+
+}
---
+++ Assets/Scripts/data/World.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World {
+
+    Tile[,] tiles;
+    int width;
+    int height;
+
+    public World(int width = 100, int height = 100){
+        this.width = width;
+        this.height = height;
+        tiles = new Tile[width, height];
+
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                tiles[x, y] = new Tile(x, y, this);
+            }
+        }
+    }
+
+    public Tile getTileAt(int x, int y) {
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            return null;
+        }
+        return tiles[x, y];
+    }
+
+}
